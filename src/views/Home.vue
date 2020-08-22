7b99e655a3906a9830a0dd46dec3c44332f1fd38
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-08-22 11:14:17 -0500

    Initial Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card
+          class="mx-auto">
+          <v-img
+            class="white--text align-end"
+            src="../assets/home/cabin-cool.jpg">
+            <v-card-title
+              style="float: right"
+              class="pb-15 pr-15">An Ode to Martha Mitchell</v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <navigation></navigation>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navigation from '@/components/Navigation.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
