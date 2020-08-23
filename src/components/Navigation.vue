355b65abe099ff5ca13f6857a06d2a9fae2126ee
JesseStew <stewed.jesse@gmail.com>
2020-08-23 10:34:56 -0500

    Carousel fix

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,6 +11,7 @@
 				:src="page.src"
 			>
 				<v-img
+						contain
 						:color="color"
 						height="100%"
 						tile
@@ -31,7 +32,7 @@
       center-active
       dark
     >
-			<v-tab @click="model = page.index - 1" :to="page.path" v-for="page in pages" :key="page.id">
+			<v-tab @click="model = page.index" :to="page.path" v-for="page in pages" :key="page.id">
 				{{ page.title }}
 			</v-tab>
     </v-tabs>
@@ -42,91 +43,91 @@
 	export default {
 		data: () => ({
 			drawer: false,
-			model: 1,
+			model: 0,
       pages: [
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/one.jpg?alt=media&token=3c27091d-a563-48fc-8120-36169b4bb561',
 					name: 'Home',
 					path: '/',
 					title: 'Home',
-					index: 1,
+					index: 0,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/legacy.jpg?alt=media&token=e8decf49-8721-49e7-af09-c92a95593242',
 					name: 'LegacyLetter',
 					path: '/legacy-letter',
 					title: 'Legacy Letter',
-					index: 2,
+					index: 1,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/intro.jpg?alt=media&token=2fa7ae19-e5b3-4532-b71f-9732a0ef8db2',
 					name: 'Intro',
 					path: '/intro',
 					title: 'Intro',
-					index: 3,
+					index: 2,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/home.jpg?alt=media&token=41c7eb6a-72ab-4e0c-b40c-bd5ceabd522b',
 					name: 'DayOne',
 					path: '/day-one',
 					title: 'Day One',
-					index: 4,
+					index: 3,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/two.jpg?alt=media&token=84711dfd-8352-4d3a-9c2d-ec76debbe8d2',
 					name: 'DayTwo',
 					path: '/day-two',
 					title: 'Day Two',
-					index: 5,
+					index: 4,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/three.jpg?alt=media&token=01469ec1-0e47-4d65-8fe4-520f6945c340',
 					name: 'DayThree',
 					path: '/day-three',
 					title: 'Day Three',
-					index: 6,
+					index: 5,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/four.jpg?alt=media&token=1afb6f39-d0c1-414f-a8c1-041682930897',
 					name: 'DayFour',
 					path: '/day-four',
 					title: 'Day Four',
-					index: 7,
+					index: 6,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/five.jpg?alt=media&token=358f4746-e66d-47dc-8610-2614a22e7730',
 					name: 'DayFive',
 					path: '/day-five',
 					title: 'Day Five',
-					index: 8,
+					index: 7,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/six-2.jpg?alt=media&token=49845f90-038a-47bb-bf07-f456324c4a2e',
 					name: 'DaySix',
 					path: '/day-six',
 					title: 'Day Six',
-					index: 9,
+					index: 8,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/time.jpg?alt=media&token=72d15a0f-81f0-4a1c-b004-8d921b034c96',
 					name: 'TimeLine',
 					path: '/time-line',
 					title: 'Time Line',
-					index: 10,
+					index: 9,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/help.jpg?alt=media&token=1f448e42-f353-4d60-924f-f5c913a5aec5',
 					name: 'HelpLines',
 					path: '/help-lines',
 					title: 'Help Lines',
-					index: 11,
+					index: 10,
         },
         {
 					src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/contact.jpg?alt=media&token=49b412eb-ee2b-4e16-9695-89dca2bf4dec',
 					name: 'Contact',
 					path: '/contact',
 					title: 'Contact',
-					index: 12,
+					index: 11,
         },
 			],
 		}),
