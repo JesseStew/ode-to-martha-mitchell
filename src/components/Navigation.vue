30e2b25833bd0cbf0df74c8eaab43030be361b5c
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-08-25 16:55:19 -0500

    Tab menu color working

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -46,7 +46,7 @@
 		</v-carousel>
 		<v-tabs
 			sticky
-			background-color="#14143a"
+			:background-color="currentColor"
 			centered
       center-active
       dark
@@ -59,6 +59,9 @@
 </template>
 
 <script>
+// colors:
+// 	brown: #845a1f, #946523
+// 	blue: #14143a
 export default {
 	computed: {
 		imgSrc() {
@@ -66,6 +69,9 @@ export default {
 		},
 		currentRoute() {
 			return this.$_.last(this.$_.split(window.location.href, '/'))
+		},
+		currentColor() {
+			return this.pages[this.model].color
 		}
 	},
 	methods: {
@@ -82,6 +88,8 @@ export default {
 		if (this.currentRoute === 'legacy-letter') {
 			console.log('hello')
 			this.model = 1
+			this.color = this.brown
+			console.log(this.color)
 		} else if (this.currentRoute === 'intro') {
 			console.log('hello')
 			this.model = 2
@@ -116,24 +124,6 @@ export default {
 			this.model = 0
 		}
 	},
-	beforeMount: function(){
-		console.log('beforeMount - currentRouteName:', this.currentRoute)
-	},
-	mounted: function(){
-		console.log('mounted - currentRouteName:', this.currentRoute)
-	},
-	beforeUpdate: function(){
-		console.log('beforeUpdate - currentRouteName:', this.currentRoute)
-	},
-	updated: function(){
-		console.log('updated - currentRouteName:', this.currentRoute)
-	},
-	beforeDestroy: function(){
-		console.log('beforeDestroy - currentRouteName:', this.currentRoute)
-	},
-	destroyed: function(){
-		console.log('destroyed - currentRouteName:', this.currentRoute)
-	},
 	watch: {
 		model: function (val) {
 			console.log('val:', val)
@@ -147,6 +137,9 @@ export default {
 	data: () => ({
 		drawer: false,
 		model: 0,
+		color: '#14143a',
+		brown: '#845a1f',
+		blue: '#14143a',
 		pages: [
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/one.jpg?alt=media&token=3c27091d-a563-48fc-8120-36169b4bb561',
@@ -156,6 +149,7 @@ export default {
 				text: 'An Ode to Martha Mitchell',
 				subtitle: 'Headwaters to COVID-19, Impeachment and Murder',
 				index: 0,
+				color: '#14143a'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/legacy.jpg?alt=media&token=e8decf49-8721-49e7-af09-c92a95593242',
@@ -163,6 +157,7 @@ export default {
 				path: '/legacy-letter',
 				title: 'Legacy Letter',
 				index: 1,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/intro.jpg?alt=media&token=2fa7ae19-e5b3-4532-b71f-9732a0ef8db2',
@@ -170,6 +165,7 @@ export default {
 				path: '/intro',
 				title: 'Intro',
 				index: 2,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/Day1_final.jpg?alt=media&token=83309fb8-5b5e-4c06-baf3-24ba21d86058',
@@ -177,6 +173,7 @@ export default {
 				path: '/day-one',
 				title: 'Day One',
 				index: 3,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/two.jpg?alt=media&token=84711dfd-8352-4d3a-9c2d-ec76debbe8d2',
@@ -184,6 +181,7 @@ export default {
 				path: '/day-two',
 				title: 'Day Two',
 				index: 4,
+				color: '#14143a'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/three.jpg?alt=media&token=01469ec1-0e47-4d65-8fe4-520f6945c340',
@@ -191,6 +189,7 @@ export default {
 				path: '/day-three',
 				title: 'Day Three',
 				index: 5,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/four.jpg?alt=media&token=1afb6f39-d0c1-414f-a8c1-041682930897',
@@ -198,6 +197,7 @@ export default {
 				path: '/day-four',
 				title: 'Day Four',
 				index: 6,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/five.jpg?alt=media&token=358f4746-e66d-47dc-8610-2614a22e7730',
@@ -205,6 +205,7 @@ export default {
 				path: '/day-five',
 				title: 'Day Five',
 				index: 7,
+				color: '#14143a'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/six-2.jpg?alt=media&token=49845f90-038a-47bb-bf07-f456324c4a2e',
@@ -212,6 +213,7 @@ export default {
 				path: '/day-six',
 				title: 'Day Six',
 				index: 8,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/time.jpg?alt=media&token=72d15a0f-81f0-4a1c-b004-8d921b034c96',
@@ -219,6 +221,7 @@ export default {
 				path: '/time-line',
 				title: 'Time Line',
 				index: 9,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/help.jpg?alt=media&token=1f448e42-f353-4d60-924f-f5c913a5aec5',
@@ -226,6 +229,7 @@ export default {
 				path: '/help-lines',
 				title: 'Help Lines',
 				index: 10,
+				color: '#845a1f'
 			},
 			{
 				src: 'https://firebasestorage.googleapis.com/v0/b/an-ode.appspot.com/o/contact.jpg?alt=media&token=49b412eb-ee2b-4e16-9695-89dca2bf4dec',
@@ -233,6 +237,7 @@ export default {
 				path: '/contact',
 				title: 'Contact',
 				index: 11,
+				color: '#845a1f'
 			},
 		],
 	}),
