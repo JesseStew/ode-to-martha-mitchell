58ae63e410baea38a669d9006491b607994479fa
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-08-26 19:27:22 -0500

    Help Line Content

--- src/views/HelpLines.vue
+++ src/views/HelpLines.vue
@@ -1,9 +1,108 @@
 <template>
-	<div>
-		<h1>
-			Help Lines
-		</h1>
-	</div>
+	<v-container>
+		<v-row>
+			<v-col cols="12">
+				<h1 class="interview-title">
+					Help Lines
+				</h1>
+			</v-col>
+			<v-col cols="12">
+				<p>
+					Australia Lifeline www.lifeline.org.au Tel 13 11 14
+				</p>
+				<p>
+					Belgium Centre de Prevention de Suicide<br />
+					www.preventionsuicide.be Hotline 0800 32 123
+				</p>
+				<p>
+					Brazil CVV Org www.cvv.org.br Tel 188
+				</p>
+				<p>
+					Canada Crisis Services Canada Tel 1 833 456 4566<br />
+					www.crisisservicescanada.ca Distress Centres www.dcontario.org<br />
+					Suicide Action Montreal Tel 514 723 4000
+				</p>
+				<p>
+					China Beijing Suicide Research and Prevention<br />
+					Hotline Tel Free 0800 810 1117 Cell Users 010 8295 1332<br />
+					www.crisis.org.cn Lifeline Shanghai (0433) 273 9595<br />
+					www.lifelineshanghai.org
+				</p>
+				<p>
+					England Samaritans www.samaritans.org/<br />
+					Tel 116.123 National Helpline Tel 111 (Option 2)
+				</p>
+				<p>
+					France SOS Help Hotline 01 46 21 46 46 Suicide Ecoute suicideecoute.pads.fr/accueil Hotline 01 45 39 40 00
+				</p>
+				<p>
+					Germany International Website Helpline Telefonseelsorge<br />
+					Deutschland National Hotline 0800 1110 111 or 0800 1110 222 www.Telefonseelsorge.org
+				</p>
+				<p>
+					Ireland Mental Health Ireland<br />
+					www.mentalhealthireland.ie Tel 01 284 1166
+				</p>
+				<p>
+					Israel Tel 1201<br />
+					www.1201.org.il www.Eran.org.il Tel 972-98891333
+				</p>
+				<p>
+					Italy Pevenire il Suicido www.preveniresuicido.it Samartans<br />
+					ONLUS Tel 800 86 00 22
+				</p>
+				<p>
+					Japan Befrienders Worldwide Osaka Suicide Prevent Center<br />
+					www.spc-osaka.org 06-6260-4343
+				</p>
+				<p>
+					Hong Kong Samaritans Hong Kong<br />
+					www.samaritans.org.hk Tel 2896 0000
+				</p>
+				<p>
+					Kenya Befrienders Kenya<br />
+					www.befrienderskenya.org Tel 254722178177
+				</p>
+				<p>
+					Netherlands 113 Online www.113.nl<br />
+					Tel 113 or 0800 0113
+				</p>
+				<p>
+					Nigeria Nigerian Suicide Prevention initiative Counseling Centers<br />
+					Tel 234 806 210 6493 Tel 234 809 210 6493
+				</p>
+				<p>
+					Norway Kirkens SOS Tel 22 40 00 40 www.kirkens-sos.no<br />
+				</p>
+				<p>
+					Russia Samaritans-Cherepovets Tel 007 (8202) 577 577<br />
+				</p>
+				<p>
+					Scotland Samaritans Tel 116 123<br />
+				</p>
+				<p>
+					South Africa Befrienders South Africa Tel 51 444 5691<br />
+				</p>
+				<p>
+					Sweden National Helpline Tel 0771 220060 Tel 020 22 00 60<br />
+					Tel (46) 31 711 2400
+				</p>
+				<p>
+					Switzerland Parspas Hotline 027 321 21 21 www.parspas.ch<br />
+				</p>
+				<p>
+					United States of America National Suicide Prevention Lifeline<br />
+					Tel 1 800 273 8255 Tel 1 800 784 2433 www.suicidepreventionlifeline.org
+				</p>
+				<p>
+					Ukraine Lifeline Ukraine Tel 7333 www.lifelineukraine.org<br />
+				</p>
+				<p>
+					Note: All phone numbers are within country calling, unless otherwise specified. If you have another country, resource number or Site not listed please inform us and we will do the best we can to add it in a timely manner. The same with corrections: email Suicidelines@thetheorem.org Special thank you to all the giving souls who work the hotlines worldwide; truly, you are our heroes.
+				</p>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
