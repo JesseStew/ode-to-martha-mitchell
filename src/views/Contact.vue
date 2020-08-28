0a36c332d9683b0b49739fc8e1181448b02fe485
Jesse Stewart <JesseDStewartDev@gmail.com>
2020-08-28 08:16:39 -0500

    Contact content

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -12,42 +12,42 @@
 				<h1 class="interview-title">
 					Contact
 				</h1>
-<p class="px-5">
-Contact us directly here at TheTheorem.org through our secure email.
-<!-- here, make sure this is correct -->
-</p>
-<p class="px-5">
-For General Inquiries, comments, corrections or suggestions.
-</p>
-<p class="px-5">
-contact@thetheorem.org TIPLINE
-<!-- here, make sure this is correct -->
-</p>
-<p class="px-5">
-If you feel in any way you have been targeted for your interest in The Theorem or in The Legacy Letter contact us at the Tipline.
-</p>
-<p class="px-5">
-All correspondence will be 100 percent confidential. Tipline@thetheorem.org
-</p>
-<p class="font-weight-medium">
-Acknowledgments &amp; Legal &amp; Media Talent
-</p>
-<p class="px-5">
-The Martha Team is:<br />
-H.L. Interviewer<br />
-L.A. Sean: Content Editor.<br />
-General Editor Good Matthew:<br />
-Lead Developer/ Web and Concept Design Miss Lisa:<br />
-Lead Graphic Artist/ Assistant Web Developer/Lead Photographer<br />
-Southpark Mike:<br />
-Lead Artist/ Graphic Artist Miss Mystica:<br />
-Editorial Assistant TiTi Tracy:<br />
-Production Assistant / Photographer Boss Potter:
-</p>
-<p class="px-5">
-Completion Financing While all team leaders feel their team is special, this group of hard-working, young people were particularly devoted to this project. Thank you so very much…Douglas
-To inquire about any one of these talented individuals above contact talent@thetheorem.org Permissions and Legal: legal@thetheorem.org
-</p>
+				<p class="px-5">
+					Contact us directly here at TheTheorem.org through our secure email.
+					<!-- here, make sure this is correct -->
+				</p>
+				<p class="px-5">
+					For General Inquiries, comments, corrections or suggestions.
+				</p>
+				<p class="px-5">
+					contact@thetheorem.org TIPLINE
+					<!-- here, make sure this is correct -->
+				</p>
+				<p class="px-5">
+					If you feel in any way you have been targeted for your interest in The Theorem or in The Legacy Letter contact us at the Tipline.
+				</p>
+				<p class="px-5">
+					All correspondence will be 100 percent confidential. Tipline@thetheorem.org
+				</p>
+					<p class="font-weight-medium">
+					Acknowledgments &amp; Legal &amp; Media Talent
+				</p>
+				<p class="px-5">
+					The Martha Team is:<br />
+					H.L. Interviewer<br />
+					L.A. Sean: Content Editor.<br />
+					General Editor Good Matthew:<br />
+					Lead Developer/ Web and Concept Design Miss Lisa:<br />
+					Lead Graphic Artist/ Assistant Web Developer/Lead Photographer<br />
+					Southpark Mike:<br />
+					Lead Artist/ Graphic Artist Miss Mystica:<br />
+					Editorial Assistant TiTi Tracy:<br />
+					Production Assistant / Photographer Boss Potter:
+				</p>
+				<p class="px-5">
+					Completion Financing While all team leaders feel their team is special, this group of hard-working, young people were particularly devoted to this project. Thank you so very much…Douglas
+					To inquire about any one of these talented individuals above contact talent@thetheorem.org Permissions and Legal: legal@thetheorem.org
+				</p>
 			</v-col>
 		</v-row>
 	</v-container>
